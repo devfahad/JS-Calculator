bce8a47b50450382b5af820204a027d89178cdec
Fahad Ahmed <mail.devfahad@gmail.com>
2019-04-09 18:02:36 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,69 +4,69 @@ Author: Fahad Ahmed
 Author URI: https://devfahad.com/
 Version: 1.1
 */
-
+'use strict';
 // Get History
-function getHistory(){
+function getHistory() {
+    
 	return document.getElementById("history-value").innerText;
 }
 
 // Print History
-function printHistory(num){
+function printHistory(num) {
 	document.getElementById("history-value").innerText = num;
 }
 
 // Get Output
-function getOutput(){
+function getOutput() {
 	return document.getElementById("output-value").innerText;
 }
 
 // Print Output
-function printOutput(num){
+function printOutput(num) {
     // If Empty, Set output-value to null
-	if(num == ""){
-		document.getElementById("output-value").innerText  =num;
-	}
-	else{
+	if (num === "") {
+		document.getElementById("output-value").innerText  = num;
+	} else {
 		document.getElementById("output-value").innerText = getFormattedNumber(num);
-	}	
+	}
 }
 
-function getFormattedNumber(num){
+function getFormattedNumber(num) {
     // For negative values, n = Number(num) returns a NaN
-	if(num == "-"){
+	if (num === "-") {
 		return "";
 	}
     // Converting string(num) to number
-	var n = Number(num);
+	let n = Number(num);
     // Converting number(n) back to string using ,(comma)
-	var value = n.toLocaleString("en");
+	let value = n.toLocaleString("en");
 	return value;
 }
 
 // Replacing comma with null and converting num string to number
-function reverseNumberFormat(num){
+function reverseNumberFormat(num) {
 	return Number(num.replace(/,/g,''));
 }
 
-var operator = document.getElementsByClassName("operator");
+let operator = document.getElementsByClassName("operator");
 // Add Click Event Listeners for operators
-for(var i = 0; i < operator.length; i++){
-	operator[i].addEventListener('click',function(){
-		if(this.id == "clear"){
+for(let i = 0; i < operator.length; i++) {
+	operator[i].addEventListener('click',function() {
+		if(this.id == "clear") {
 			printHistory("");
 			printOutput("");
 		}
-		else if(this.id == "backspace"){
+		else if(this.id == "backspace") {
             // Converting comma output-value string to a number using reverseNumberFormate and then to string
-			var output = reverseNumberFormat(getOutput()).toString();
+			let output = reverseNumberFormat(getOutput()).toString();
 			if(output){// If output has a value
 				output = output.substr(0,output.length-1);
 				printOutput(output);
 			}
 		}
 		else{
-			var output = getOutput();
-			var history = getHistory();
+			let output = getOutput();
+			let history = getHistory();
 			if(output == "" && history != ""){ // If output empty and history not empty
 				if(isNaN(history[history.length-1])){ // Changing Operator in the last index
 					history = history.substr(0,history.length-1);
@@ -78,7 +78,7 @@ for(var i = 0; i < operator.length; i++){
                 // Concatanating each output-value(number) to history-value(string) resulting string
 				history = history + output;
 				if(this.id == "="){
-					var result = eval(history); // eval(string)
+					let result = eval(history); // eval(string)
 					printOutput(result);
 					printHistory("");
 				}
@@ -92,11 +92,11 @@ for(var i = 0; i < operator.length; i++){
 		
 	});
 }
-var number = document.getElementsByClassName("number");
+let number = document.getElementsByClassName("number");
 // Add Click Event Listeners for numbers
-for(var i = 0; i < number.length; i++){
+for(let i = 0; i < number.length; i++){
 	number[i].addEventListener('click',function(){
-		var output = reverseNumberFormat(getOutput());
+		let output = reverseNumberFormat(getOutput());
 		if(output != NaN){ // If output is a number
             // Concatanating output(number) with this.id i.e. clicked output-value(string) resulting string
 			output = output + this.id;
